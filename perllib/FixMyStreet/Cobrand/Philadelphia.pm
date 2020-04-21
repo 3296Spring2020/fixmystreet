2b84d7ca9f00041b3cb52d24f2e2d8a5748a9c64
Stephen Lucas <tuf59191@temple.edu>
2020-04-20 22:28:53 -0400

    Updated cobrand config, changed logo.

--- perllib/FixMyStreet/Cobrand/Philadelphia.pm
+++ perllib/FixMyStreet/Cobrand/Philadelphia.pm
@@ -8,15 +8,3 @@ sub get_geocoder { 'Philadelphia' }
 
 
 sub on_map_default_status { 'open' }
-
-sub pin_colour {
-    my ( $self, $p, $context ) = @_;
-    return 'grey' if $p->is_closed;
-    return 'green' if $p->is_fixed;
-    return 'yellow' if $p->state eq 'confirmed';
-    return 'orange'; # all the other `open_states` like "in progress"
-}
-
-sub path_to_pin_icons {
-    return '/cobrands/philadelphia/images/';
-}
